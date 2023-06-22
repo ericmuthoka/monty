25592426831d0cfef746ff4b6f600ca4e41de851
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 12:33:25 -0700

    stacks and queues

--- pint.c
+++ pint.c
@@ -16,7 +16,7 @@ void pint(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		free(*head);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
