66fa79ae1fda0d94e58801eba2be51ce5f146dfc
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 13:59:02 -0700

    stacks and queues

--- pint.c
+++ pint.c
@@ -6,12 +6,15 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
+	stack_t *top;
+
 	if (*stack == NULL)
 	{
-		printf("L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("%d\n", (*stack)->n);
+	top = *stack;
+	printf("%d\n", top->n);
 }
 
