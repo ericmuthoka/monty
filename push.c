66e040ea7b883ebd8f61bb7030c4ad5a9350e3f7
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 10:54:49 -0700

    stacks and queues

--- push.c
+++ push.c
@@ -21,29 +21,4 @@ void push(stack_t **stack, unsigned int line_number)
 		stack_push(stack, value);
 	}
 }
-/**
- * pall - Prints all values in the stack.
- * @stack: Pointer to a pointer to the stack.
- * @line_number: The line number where the pall operation is being performed.
- * Return: No return value.
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-	{
-		if (stack_is_empty(stack))
-		{
-			return;
-		}
-
-		stack_t *node = stack_top(stack);
-
-		while (node != NULL)
-		{
-			printf("%d\n", node->n);
-			node = node->prev;
-		}
-	}
-}
-
-
 
