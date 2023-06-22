d3d87416865a12afb3a3a0f73a98dd1f81b13b8b
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 13:31:44 -0700

    stacks and queues

--- pop.c
+++ pop.c
@@ -7,13 +7,14 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp;
 	if (*stack == NULL)
 	{
 		printf("L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	stack_t *temp = *stack;
+	temp = *stack;
 	*stack = (*stack)->next;
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
