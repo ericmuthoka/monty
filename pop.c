1ea982a97fc256cd5fa5b823b3580262344a92b9
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 13:26:46 -0700

    stacks and queues

---
+++ pop.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * pop - Removes the top element of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number where the pop opcode is encountered.
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		printf("L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
+}
+
