8d99dbc6607a4510331f581ae68a8fbadfd56520
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 14:09:49 -0700

    stacks and queues

---
+++ swap.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Double pointer to the stack's head.
+ * @line_number: Line number of the instruction being executed.
+ *
+ * Description: If the stack contains fewer than two elements,
+ * it prints an error message and exits with status EXIT_FAILURE.
+ * Otherwise, it swaps the values of the top two elements on the stack.
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *second;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	top = *stack;
+	second = top->next;
+
+	/* Swap the values of top and second */
+	int temp = top->n;
+
+	top->n = second->n;
+	second->n = temp;
+}
+
