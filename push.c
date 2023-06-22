e0ce37e507fe6cc4c75fbced3eee0bcc64c4d0ea
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 11:11:46 -0700

    stacks and queues

--- push.c
+++ push.c
@@ -5,10 +5,9 @@
  * @line_number: The line number where the push operation is being performed.
  * Return: No return value.
  */
-void push(stack_t **stack, unsigned int line_number)
+static void push(stack_t **stack, unsigned int line_number)
 {
-	if (!bus.arg)
-	{
+	if (!bus.arg) {
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		fclose(bus.file);
 		free(bus.content);
@@ -16,10 +15,7 @@ void push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	{
-		int value = atoi(bus.arg);
-
-		stack_push(stack, value);
-	}
+	int value = atoi(bus.arg);
+	stack_push(stack, value, line_number);
 }
 
