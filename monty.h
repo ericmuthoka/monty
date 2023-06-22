1558954a3876df54c12293d95b22ebeb3ee12b03
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 11:14:02 -0700

    stacks and queues

--- monty.h
+++ monty.h
@@ -34,7 +34,6 @@ typedef struct instruction_s
 #include <stdio.h>
 #include <stdlib.h>
 
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+static void push(stack_t **stack, unsigned int line_number);
 
 #endif
