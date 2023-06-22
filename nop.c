403f15cf21256decf11e3618cf77b2ee19ad04a0
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 14:37:54 -0700

    stacks and queues

---
+++ nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Double pointer to the stack's head.
+ * @line_number: Line number of the instruction being executed.
+ *
+ * Description: This opcode doesn't perform any action.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	/* No operation */
+}
+
