0492905ea6c76a7159509969cb9072839c4c568a
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 12:12:54 -0700

    stacks and queues

--- monty.h
+++ monty.h
@@ -1,6 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -12,9 +20,9 @@
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,16 +35,34 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode function
+ */
 
-#include <unistd.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
+void push(stack_t **stack, unsigned int line_number);
 
-static void push(stack_t **stack, unsigned int line_number);
+#endif /* MONTY_H */
 
-#endif
--- push.c
+++ push.c
@@ -1,21 +1,44 @@
 #include "monty.h"
 /**
- * push - Adds a node to the stack.
- * @stack: Pointer to a pointer to the stack.
- * @line_number: The line number where the push operation is being performed.
- * Return: No return value.
+ * push - push an integer onto the stack
+ * @stack: pointer to the stack
+ * @line_number: line number in the file
+ *
+ * Description:
+ *   - parses the second argument as an integer
+ *   - pushes the integer onto the stack
  */
-static void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number)
 {
-	if (!bus.arg) {
+	int flag = 0;
+	int i = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+		{
+			i++;
+		}
+		for (; bus.arg[i] != '\0'; i++)
+		{
+			if (bus.arg[i] > 57 || bus.arg[i] < 48)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
-	int value = atoi(bus.arg);
-	stack_push(stack, value, line_number);
+	push(stack, line_number);
 }
 
