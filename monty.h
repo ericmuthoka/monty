90ba144b4d1d035af1cc29bbdcf1efbb95d66193
ericmuthoka <muthokaerick@gmail.com>
2023-06-22 18:09:48 -0700

    School

--- monty.h
+++ monty.h
@@ -51,5 +51,7 @@ void pchar(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack);
 void rotr(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+
 
 #endif
